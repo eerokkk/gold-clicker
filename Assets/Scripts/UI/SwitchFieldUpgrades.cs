cb9eb3ed10431403872264693afe39a29d98a7b1
eerokkk <eerok228@gmail.com>
2020-08-04 19:34:23 +0500

    Добавил мультиплай кнопку (внутри неё переменная) и 3 рекламные кнопки

--- Assets/Scripts/UI/SwitchFieldUpgrades.cs
+++ Assets/Scripts/UI/SwitchFieldUpgrades.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using uClicker;
 using UnityEngine;
 
 public class SwitchFieldUpgrades : MonoBehaviour
 {
     public GameObject FieldEquipment;
     public GameObject FieldUpgrades;
+    public TextMeshProUGUI MultiplyText;
+    public ClickerManager GoldManager;
 
     [SerializeField]
     private bool Activity = true;
+    private int Count = 1;
+    private int BuyMultiply;
 
 public void switchFieldEquipment()
     {
@@ -37,5 +42,33 @@ public void switchFieldUpgrades()
             case false:
                 break;
         }
+    }
+
+    public void changeTextInMultiplyButton()
+    {
+        switch (Count)
+        {
+            case 0:
+                BuyMultiply = 1;
+                MultiplyText.text = $"x{BuyMultiply}";
+                Count++;
+                break;
+            case 1:
+                BuyMultiply = 10;
+                MultiplyText.text = $"x{BuyMultiply}";
+                Count++;
+                break;
+            case 2:
+                BuyMultiply = 100;
+                MultiplyText.text = $"x{BuyMultiply}";
+                Count++;
+                break;
+            case 3:
+                MultiplyText.text = "Max";
+                Count = 0;
+                break;
+        }
+
+        GoldManager.OnTick.Invoke();
     }
 }
